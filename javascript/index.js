36de6de432d0b5b4a0dd565ca5cac06a31b4de10
AngelSCodes <158100915+AngelSCodes@users.noreply.github.com>
2024-06-08 21:33:31 +0200

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -48,6 +48,9 @@ setInterval(displayTime, 1000);
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
